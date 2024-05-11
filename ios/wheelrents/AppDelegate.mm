8e47b92709ca1f8d0fea8d32d7221407b49b5ff9
ravikar sahay <ravikarsahay55@gmail.com>
2024-05-11 21:28:42 +0530

    new changes for ios

--- ios/wheelrents/AppDelegate.mm
+++ ios/wheelrents/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -9,6 +11,7 @@
   self.moduleName = @"wheelrents";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
+  [FIRApp configure];
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
